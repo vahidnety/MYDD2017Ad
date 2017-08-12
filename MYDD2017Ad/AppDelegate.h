f2d00039da46ad09e1adc8620ddf09fb01edcd51
Seyedvahid Dianat <Vahid@Yasamans-MacBook-Pro.local>
2017-08-12 08:52:44 +0800

    Initial Commit

---
+++ MYDD2017Ad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYDD2017Ad
+//
+//  Created by Seyedvahid Dianat on 12/08/2017.
+//  Copyright © 2017 Seyedvahid Dianat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYDD2017Ad/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MYDD2017Ad
+//
+//  Created by Seyedvahid Dianat on 12/08/2017.
+//  Copyright © 2017 Seyedvahid Dianat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
